2fdc6efed1e8584caff879e63010670707bafdf0
FranziAE <148230994+FranziAE@users.noreply.github.com>
2025-04-14 10:28:21 +0200

    kaputter endpoint test ansatz

---
+++ src/test/java/com/nomnom/nnws/project/controller/HealthControllerTest.java
@@ -0,0 +1,28 @@
+package com.nomnom.nnws.project.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@WebMvcTest(HealthController.class)
+public class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    // ist kaputt :c
+    @Test
+    public void getHealth() 
+        throws Exception {
+            this.mvc.perform(get("/health").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+
+    }
+    
+}
