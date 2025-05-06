c599743ace0216b66ed6dc790d33fa63b6c59abb
FranziAE <148230994+FranziAE@users.noreply.github.com>
2025-05-06 17:50:52 +0200

    hotfix oder so ahhhh

--- src/main/java/com/nomnom/nnws/project/repository/AllergenIngredientRepository.java
+++ src/main/java/com/nomnom/nnws/project/repository/AllergenIngredientRepository.java
@@ -1,11 +1,13 @@
 package com.nomnom.nnws.project.repository;
 
 import com.nomnom.nnws.project.entity.AllergenIngredient;
+import com.nomnom.nnws.project.entity.AllergenIngredientId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Set;
 
-public interface AllergenIngredientRepository extends JpaRepository<AllergenIngredient, Long> {
+public interface AllergenIngredientRepository extends JpaRepository<AllergenIngredient, AllergenIngredientId> {
     List<AllergenIngredient> findByAllergenIdIn(Set<Long> allergenIds);
 }
